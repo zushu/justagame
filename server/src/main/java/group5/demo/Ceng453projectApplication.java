5a7db9b56a091271b5293472c56d64bf6d1e652b
e2171676 <eeraykarabulutt@gmail.com>
2020-02-29 17:23:54 +0300

    db connection, spring bootstapped

---
+++ server/src/main/java/group5/demo/Ceng453projectApplication.java
@@ -0,0 +1,13 @@
+package group5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceng453projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ceng453projectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/group5/demo/Ceng453projectApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ceng453projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
