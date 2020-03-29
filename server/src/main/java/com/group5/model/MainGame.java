c1cdddd85132ea4c03e4816c92244f3b18ddd822
eray.karabulut <eeraykarabulutt@gmail.com>
2020-03-29 23:05:53 +0300

    MainGame class created.

--- server/src/main/java/com/group5/model/MainGame.java
+++ server/src/main/java/com/group5/model/MainGame.java
@@ -2,4 +2,32 @@ package com.group5.model;
 
 public class MainGame{
     //main gameplay will go here
+    private Integer gameLevel;
+    private Grid grid;
+
+    public MainGame(){
+        this.gameLevel = 1;
+    }
+
+    public MainGame(Grid grid){
+        this.grid = grid;
+    }
+
+    public Grid getGrid(){
+        return this.grid;
+    }
+
+    public Integer getGameLevel(){
+        return this.gameLevel;
+    }
+
+    public void incrementGameLevel(){
+        this.gameLevel++;
+    }
+
+    public void gamePlay(){
+
+        Grid grid = 
+    }
+
 }
