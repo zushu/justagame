d373745c6e51feb0f54c85e737db8d79bfcf3535
eray.karabulut <eeraykarabulutt@gmail.com>
2020-03-30 01:01:09 +0300

    general game logic implementation in MainGame class and some changes on shooting objects

--- server/src/main/java/com/group5/model/Grid.java
+++ server/src/main/java/com/group5/model/Grid.java
@@ -8,15 +8,17 @@ public class Grid {
     private double width;                       // number of cells horizontally
     private double height;                      // number of cells vertically
     private List<SpaceShip> spaceShipList;      // one element in single player mode
-    private List<Alien> alienList;
+    private List<IAlien> alienList;
+    private Integer gameLevel;
 
     public Grid() {}
-    public Grid(double width, double height, List<SpaceShip> spaceShipList, List<Alien> alienList)
+    public Grid(double width, double height, List<SpaceShip> spaceShipList, List<IAlien> alienList)
     {
         this.width = width;
         this.height = height;
         this.spaceShipList = spaceShipList;
         this.alienList = alienList;
+        this.gameLevel = 1;
     }
 
     public double getWidth() {
@@ -31,8 +33,20 @@ public class Grid {
         return spaceShipList;
     }
 
-    public List<Alien> getAlienList() {
+    public List<IAlien> getAlienList() {
         return alienList;
+    }
+
+    public Integer getGameLevel(){
+        return this.gameLevel;
+    }
+
+    public void resetGameLevel(){
+        this.gameLevel = 1;
+    }
+
+    public void incrementGameLevel(){
+        this.gameLevel++;
     }
 
     public void setWidth(double width) {
@@ -47,7 +61,7 @@ public class Grid {
         this.spaceShipList = spaceShipList;
     }
 
-    public void setAlienList(List<Alien> alienList) {
+    public void setAlienList(List<IAlien> alienList) {
         this.alienList = alienList;
     } 
 
--- server/src/main/java/com/group5/model/SpaceShip.java
+++ server/src/main/java/com/group5/model/SpaceShip.java
@@ -10,12 +10,16 @@ import java.util.Iterator;
 public class SpaceShip extends AliveGridObject {
     private double shootFrequency; // can change
     private List<Bullet> bulletList;
+    private Bullet bulletType;
+
     public SpaceShip() {}
-    public SpaceShip(Vector2D position, Vector2D direction, double speed, double width, double height, List<Bullet> bulletList, double shootFrequency, double health)
+    public SpaceShip(Vector2D position, Vector2D direction, double speed, double width, double height, 
+                    List<Bullet> bulletList, double shootFrequency, double health, Bullet bulletType)
     {
         super(position, direction, speed, width, height, health);
         this.bulletList = bulletList; // TODO: check, is it correct ???     this may make just references equal, so we may need deep copy
         this.shootFrequency = shootFrequency;
+        this.bulletType = bulletType;
     }
 
     public List<Bullet> getBulletList() {
@@ -34,11 +38,19 @@ public class SpaceShip extends AliveGridObject {
         this.shootFrequency = shootFrequency;
     }  
 
+    public Bullet getBulletType(){
+        return this.bulletType;
+    }
+
     // TODO: CALL IT IN A THREAD WITH SLEEPS USING shootFrequency
-    public void shootOneBullet(double bulletSpeed, double bulletWidth, double bulletHeight, double bulletDamage) {
-        Vector2D bulletPosition = new Vector2D(this.getPosition().x, this.getUpperBoundary() + bulletHeight / 2.0d);
-        Vector2D bulletDirection = new Vector2D(0.0d, 1.0d); // upwards
-        Bullet newBullet = new Bullet(bulletPosition, bulletDirection, bulletSpeed, bulletWidth, bulletHeight, bulletDamage);
+    public Vector2D getBulletsInitialPosition(){
+        Vector2D bulletsInitialPosition = new Vector2D(this.getPosition().x, this.getLowerBoundary() - this.getBulletType().getHeight() / 2.0d);
+        return bulletsInitialPosition;
+    }
+
+    public void shootOneBullet() {
+        Vector2D upVector = new Vector2D(0.0d, 1.0d); // upwards
+        Bullet newBullet = new Bullet(this.getBulletsInitialPosition(), upVector, this.getBulletType().getSpeed(), this.getBulletType().getWidth(), this.getBulletType().getHeight(), this.getBulletType().getDamage());
         bulletList.add(newBullet);
     }
 
