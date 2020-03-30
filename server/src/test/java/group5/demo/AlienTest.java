5b2c4b0aabf3427a2a7b456800db03686513bc99
Zumrud Shukurlu <zumrudshukurlu@pc-4-11.wls.metu.edu.tr>
2020-03-30 04:00:32 +0300

    decorated alien tests

--- server/src/test/java/group5/demo/AlienTest.java
+++ server/src/test/java/group5/demo/AlienTest.java
@@ -4,6 +4,7 @@ import com.group5.model.Alien;
 import com.group5.model.IAlien;
 import com.group5.model.DefensiveAlien;
 import com.group5.model.ShootingAlien;
+import com.group5.model.Bullet;
 import com.group5.helper.Vector2D;
 
 import org.junit.jupiter.api.Test;
@@ -18,17 +19,22 @@ public class AlienTest {
 
     Vector2D position = new Vector2D(2.0d, 2.0d);
     Vector2D direction = new Vector2D(0.0d, 1.0d);
+    Alien gridObject1; // extends GridObject
     IAlien gridObject; // normal Alien
-    //IAlien shootingAlien;
-    //IAlien defensiveAlien;
-    //IAlien defensiveShootingAlien;
+    IAlien shootingAlien;
+    IAlien defensiveAlien;
+    IAlien defensiveShootingAlien;
 
     @BeforeEach
     public void init() {
-        Vector2D position = new Vector2D(2.0d, 2.0d);
-        Vector2D direction = new Vector2D(0.0d, 1.0d);
+        //Vector2D position = new Vector2D(2.0d, 2.0d);
+        //Vector2D direction = new Vector2D(0.0d, 1.0d);
+        Bullet bulletType = new Bullet();
         gridObject = new Alien(position, direction, 1.0d, 2.0d, 2.0d, 100.0d);
-        //shootingAlien = new 
+        gridObject1 = new Alien(position, direction, 1.0d, 2.0d, 2.0d, 100.0d);
+        shootingAlien = new ShootingAlien(new Alien(position, direction, 1.0d, 2.0d, 2.0d, 100.0d), 5, bulletType);
+        defensiveAlien = new DefensiveAlien(new Alien(position, direction, 1.0d, 2.0d, 2.0d, 100.0d));
+        defensiveShootingAlien = new DefensiveAlien(new ShootingAlien(new Alien(position, direction, 1.0d, 2.0d, 2.0d, 100.0d), 5, bulletType));
     }
 
     @Test
@@ -68,7 +74,6 @@ public class AlienTest {
 
     @Test
     public void testDoCollide() {
-        Alien gridObject1 = new Alien(new Vector2D(2.0d, 2.0d), new Vector2D(0.0d, 1.0d), 1.0d, 2.0d, 2.0d, 100.0d);
         IAlien gridObject2 = new Alien(new Vector2D(3.0d, 3.0d), new Vector2D(0.0d, 1.0d), 1.0d, 2.0d, 2.0d, 100.0d);
         boolean result = gridObject.doCollide(gridObject1, gridObject2);
         boolean checkResult = true;
@@ -83,6 +88,28 @@ public class AlienTest {
         gridObject.getHit(50.0d);
         assertEquals(false, gridObject.getAlive());
         assertEquals(0.0d, gridObject.getHealth());
+    }
+
+    @Test 
+    public void testDecoratedAliens() {
+        shootingAlien.setPosition(new Vector2D(5.0d, 5.0d));
+        defensiveAlien.setPosition(new Vector2D(3.0d, 3.0d));
+        defensiveShootingAlien.setPosition(new Vector2D(1.0d, 1.0d));
+
+
+        /*double left = shootingAlien.getLeftBoundary();
+        assertEquals(4.0d, left);
+        left = defensiveAlien.getLeftBoundary();
+        assertEquals(2.0d, left);
+*/
+        boolean result = gridObject1.doCollide(gridObject1, shootingAlien);
+        assertEquals(false, result);
+        /*result = defensiveAlien.doCollide(defensiveAlien, gridObject1);
+        assertEquals(true, result);
+        result = defensiveShootingAlien.doCollide(defensiveShootingAlien, gridObject1);
+        assertEquals(false, result);
+*/
+
     }
 }
 
